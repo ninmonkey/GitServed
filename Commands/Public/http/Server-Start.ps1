8d37c54c1e0f43eab0ecc82f2c333a860dd0a302
Jake Bolton <ninmonkeys@gmail.com>
2026-06-06 10:21:39 -0500

    add `$ModuleState` and Commands `Server-Start.ps1`, `Server-Stop.ps1`

---
+++ Commands/Public/http/Server-Start.ps1
@@ -0,0 +1,46 @@
+﻿function Start-GitServe {  # 'Start-GitServeServer' sounded bad
+    <#
+    .synopsis
+        Start listen server
+    .notes
+        aliased as Start-GitServe, GitServe.Start
+    .example
+        # random ports
+        > GitServe Start
+
+    .example
+        > GitServe Start -Host ip -Port port
+    Maybe allow:
+        > GitServe Start ip:port
+        > GitServe Start port
+    .LINK
+        Start-GitServe
+    .LINK
+        Stop-GitServe
+    .LINK
+        GitServe
+    #>
+    [Alias('GitServe.Start')]
+    # [OutputType( [string] )]
+    [CmdletBinding()]
+    param(
+        [ArgumentCompletions(
+            "'127.0.0.1'", "'*'", "'localhost'" # "'0.0.0.0'", "'*'", "'localhost'"
+        )]
+        [Alias('Ip')]
+        [Parameter(Position = 0)]
+        [String] $Host,
+
+        [Parameter()]
+        [int] $Port
+    )
+
+    if( -not $Port ) {
+        $Port = Get-Random -Minimum 3000 -Maximum 4000
+    }
+
+    $script:ModuleState.HostName = $Host
+    $script:ModuleState.Port = $Port
+    "GitServe: started listening on: ${Host}:${Port} at $( (Get-Date).ToString('u'))"
+        | Write-Host
+}
---
+++ Commands/Public/http/Server-Stop.ps1
@@ -0,0 +1,33 @@
+﻿function Stop-GitServe {  # 'Stop-GitServeServer' sounded bad
+    <#
+    .synopsis
+        Stop listen server
+    .notes
+        aliased as Stop-GitServe, GitServe.Stop
+    .example
+        # random ports
+        > GitServe Stop
+
+    .example
+        > GitServe Stop -Host ip -Port port
+    Maybe allow:
+        > GitServe Stop ip:port
+        > GitServe Stop port
+    .LINK
+        Stop-GitServe
+    .LINK
+        Stop-GitServe
+    .LINK
+        GitServe
+    #>
+    [Alias('GitServe.Stop')]
+    [CmdletBinding()]
+    param( )
+
+    if( -not $Port ) {
+        $Port = Get-Random -Minimum 3000 -Maximum 4000
+    }
+    $msg = '{0}:{1}' -f ( $script:ModuleState.HostName, $script:ModuleState.Port )
+    "GitServe: Stopped listening on: ${msg} at $( (Get-Date).ToString('u'))"
+        | Write-Host
+}
