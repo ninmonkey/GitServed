1aba688016c0e6785ba44a4b3ac062a8fe615abc
Jake Bolton <ninmonkeys@gmail.com>
2026-06-22 09:56:22 -0500

    add: test script for  `/repo/list`

---
+++ Tests/scripts/test_repo_list.ps1
@@ -0,0 +1,23 @@
+﻿<#
+.SYNOPSIS
+    Tests the endpoint: /repo/list
+.EXAMPLE
+    ./test_repo_list.ps1
+#>
+param(
+    [int] $Port = 3001,
+    [string] $HostName = 'http://127.0.0.1'
+)
+
+$UrlPrefix = "${HostName}:${Port}"
+$QueryPath = 'repo/list'
+
+[uri] $UrlRequest = "${UrlPrefix}/${QueryPath}"
+
+( $resp = irm $UrlRequest -RetryIntervalSec 1 -MaximumRetryCount 1 -SkipHttpErrorCheck ) | ft
+
+'## Vars' | Write-Host -fg Cyan
+Get-Variable '*Url*', 'Resp' | ft -auto
+
+'## $resp = Invoke-RestMethod -Url "{0}"' -f $UrlRequest.ToString() | Write-Host -fg cyan
+$resp |Json | jq '. | keys'
