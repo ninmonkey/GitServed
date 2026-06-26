7d91721ffb48b770e03bf285ac588df3034c8aae
Jake Bolton <ninmonkeys@gmail.com>
2026-06-26 11:22:51 -0500

    add: Metric CommitCount ( closes: #12 Commit: ByMonthAuthor )
    adds new `.psd1` exports:
    - functions: 'Metric-*'
    - aliases: 'GitServe.*'

---
+++ Commands/Public/metric/CommitCount.ps1
@@ -0,0 +1,65 @@
+﻿function Metric-GitServeCommitCount {
+    <#
+    .SYNOPSIS
+        Number of commits grouped and sorted by: "<Year>-<Month>_<GitUserName>" as text Descending
+    .NOTES
+        Expects input type: 'git.log'
+    .EXAMPLE
+        git log | Metric-CommitCount
+    .EXAMPLE
+        Use-Git -GitArg 'log', '-n', 4, '-C', $path | GitServe.Metric.CommitCount
+    #>
+    [Alias('GitServe.Metric.CommitCount')]
+    [OutputType(
+        '[System.Collections.Generic.SortedDictionary[string,object]]'
+    )]
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [DateTime] $CommitDate,
+
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string] $GitUserName,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string] $GitUserEmail,
+
+        # Input has 'ugit' properties like 'git.log'
+        [Parameter(ValueFromPipeline)]
+        [object] $InputObject
+    )
+    begin {
+        function __toKeyId {
+            # Generate a PrimaryKey. This determines distinct testing for records
+            param( $Obj )
+            '{0}_{1}' -f @(
+                $Obj.CommitDate.ToString('yyyy-MM')
+                $Obj.GitUserName
+            )
+        }
+        $reverseComparer = [System.Collections.Generic.Comparer[string]]::Create({
+            param($x, $y) [string]::Compare($y, $x)
+        })
+        [Collections.Generic.SortedDictionary[string,object]] $metric = $reverseComparer
+
+        #//@{}
+    }
+    process {
+        $key = __toKeyId $InputObject
+        if( -not $metric.ContainsKey( $key ) ) {
+            $initialValue = [pscustomobject]@{
+                PSTYpeName = 'GitServe.Metric.CommitCount'
+                CommitDate  = $CommitDate
+                GitUserName = $GitUserName
+                CommitCount = 1
+                GroupBy     = $key
+            }
+            $metric[ $key ] = $initialValue
+        } else {
+            $metric[ $key ].CommitCount += 1
+        }
+    }
+    end {
+        $metric
+    }
+}
--- GitServed.psd1
+++ GitServed.psd1
@@ -71,13 +71,18 @@
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
     FunctionsToExport = @(
         # '*'
+        # '*-GitServe*'
+        # 'GitServe.Metric.*'
         '*-GitServe*'
+        'Metric-*'
     )
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport   = @(
         # '*'
-        '*-GitServe*'
+        # '*-GitServe*'
+        # 'GitServe.Metric.*'
+        # 'Metric-*'
 
     )
 
@@ -86,7 +91,8 @@
 
     # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
     AliasesToExport   = @(
-        # 'GitServe.*'
+        'GitServe.*'
+        # 'GitServe.Metric.*'
     )
 
     # DSC resources to export from this module
