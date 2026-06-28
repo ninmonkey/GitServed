320ca7cd4806b7c29ee2ae6416f946d31ad0a11f
Jake Bolton <ninmonkeys@gmail.com>
2026-06-28 12:04:23 -0500

    add: Route to display debug info on cached responses

---
+++ Routes/Public/debug/cache/list.ps1
@@ -0,0 +1,18 @@
+﻿function /cache/list {
+    <#
+    .SYNOPSIS
+        Debug. Displays metadata on cached responses
+    .description
+        Basic info on the state of '$Script:ResponseCache'
+    #>
+    [OutputType( 'GitServe.Route.Cache.List' )]
+    param()
+    $cache = $Script:ResponseCache
+    $cache.GetEnumerator() | %{
+        [pscustomobject][ordered]@{
+            PSTypeName = 'GitServe.Route.Cache.List'
+            Key        = $_.Key
+            ValueType  = $_.Value | % GetType | % Name | Sort-Object -unique | Join-String -sep ', '
+        }
+    }
+}
