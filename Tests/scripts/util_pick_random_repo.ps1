690b7fce367ba15abf3c52128dfde906c44745e9
Jake Bolton <ninmonkeys@gmail.com>
2026-06-28 11:29:55 -0500

    add: Testing utils

---
+++ Tests/scripts/util_pick_random_repo.ps1
@@ -0,0 +1,24 @@
+﻿#requires -Module Mintils
+
+function _PickRepo {
+    <#
+    .synopsis
+       (debug util). quickly swap example repos
+    .example
+        ($out = git log -C (_PickRepo) | Metric-GitServeCommitCount ) | ft -AutoSize
+    #>
+    [CmdletBinding()]
+    param(
+        # Root folder of cloned repos
+        [ArgumentCompletions( 'H:\RootClonedRepos', '.' )]
+        [Parameter()]
+        [string] $BaseRepoPath = 'H:/RootClonedRepos'
+    )
+    $choices = Mint.Find-GitRepository -BaseDirectory $BaseRepoPath -MaxDepth 4 -PassTHru
+    $pick = $choices.fullName | fzf | Get-Item
+    '$SelectedGitRepo = {0}' -f $pick | Write-Host -fg 'SlateGray'
+
+    # save and emit new repo path
+    $script:SelectedGitRepo  = $pick
+    $pick
+ }
