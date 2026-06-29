8fc1cd5619104ff19b68e4f941ad9d439f826b09
Jake Bolton <ninmonkeys@gmail.com>
2026-06-29 08:40:20 -0500

    clean: ensure nativegit is explicit when used

--- Routes/Public/repo/list.ps1
+++ Routes/Public/repo/list.ps1
@@ -15,13 +15,14 @@
     $searchRoot = @( GetConfig.ClonedRepoRoot )
     $findGitRepos = Get-ChildItem $searchRoot -Filter '.git' -Directory -Force -Recurse | ForEach-Object Parent
 
+
     $records = @(
         foreach ($repoPath in $findGitRepos) {
             $absolutePath = $repoPath.FullName
-            $remote = ( & git -C $absolutePath remote get-url origin 2>$null ) ?? '<empty-remote>'
-            $commitCount = ( & git -C $absolutePath rev-list --count HEAD )
-            $newestCommitRelative = ( & git -C $absolutePath log -1 --format=%cr )
-            $newestCommitDateOnly = ( & git -C $absolutePath log -n 1 --format=%cd --date=format:'%Y-%m-%d' )
+            $remote = ( gitreal -C $absolutePath remote get-url origin 2>$null ) ?? '<empty-remote>'
+            $commitCount = ( gitreal -C $absolutePath rev-list --count HEAD )
+            $newestCommitRelative = ( gitreal -C $absolutePath log -1 --format=%cr )
+            $newestCommitDateOnly = ( gitreal -C $absolutePath log -n 1 --format=%cd --date=format:'%Y-%m-%d' )
             $ownerPathName = $repoPath.FullName | Split-path -Parent | split-path  -Leaf
 
             [pscustomobject][ordered]@{
