734fceae0ee8fcf0ecd053877fe6d38321053437
Jake Bolton <ninmonkeys@gmail.com>
2026-07-13 07:12:23 -0500

    add: add parameters to CommitCount for aggregation Period

--- Commands/Public/metric/CommitCount.ps1
+++ Commands/Public/metric/CommitCount.ps1
@@ -6,6 +6,7 @@
         Expects input type: 'git.log'
     .EXAMPLE
         git log | Metric-CommitCount
+        git log | Metric-GitServeCommitCount -Period month
     .EXAMPLE
         Use-Git -GitArg 'log', '-n', 4, '-C', $path | GitServe.Metric.CommitCount
     #>
@@ -26,36 +27,45 @@
 
         # Input has 'ugit' properties like 'git.log'
         [Parameter(ValueFromPipeline)]
-        [object] $InputObject
+        [object] $InputObject,
+
+        # Period to aggregate by: 'year' | 'month' | 'day'
+        [ValidateSet('month', 'day', 'year')]
+        [string] $Period = 'month'
     )
     begin {
+        $dateDisplayFormat = 'yyyy-MM-dd'
+        switch( $Period ) {
+            'year' { $keyFormat  = 'yyyy' }
+            'month' { $keyFormat  = 'yyyy-MM' }
+            'day' { $keyFormat  = 'yyyy-MM-dd' }
+            default { throw "Invalid Period! ${Period}" }
+        }
         function __toKeyId {
             # Generate a PrimaryKey. This determines distinct testing for records
             param( $Obj )
             '{0}_{1}' -f @(
-                $Obj.CommitDate.ToString('yyyy-MM')
+                $Obj.CommitDate.ToString( $keyFormat )
                 $Obj.GitUserName
             )
         }
         $reverseComparer = [System.Collections.Generic.Comparer[string]]::Create({
             param($x, $y) [string]::Compare($y, $x)
-        })
+        }) # todo: use numeric date sort
         [Collections.Generic.SortedDictionary[string,object]] $metric = $reverseComparer
-
-        #//@{}
     }
     process {
         $key = __toKeyId $InputObject
         if( -not $metric.ContainsKey( $key ) ) {
             $initialValue = [pscustomobject][ordered]@{
                 PSTYpeName  = 'GitServe.Metric.CommitCount'
-                DateString  = $CommitDate.ToString('yyyy-MM')
+                DateDisplay  = $CommitDate.ToString( $dateDisplayFormat )
                 GitUserName = $GitUserName
                 CommitCount = 1
                 Year        = $CommitDate.Year
                 Month       = $CommitDate.Month
-                KeyId       = $key
                 CommitDate  = $CommitDate
+                KeyId       = $key
             }
             $metric[ $key ] = $initialValue
         } else {
