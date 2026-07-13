bcd205a313e199320caaff4de3a78b3e26dad0fb
Jake Bolton <ninmonkeys@gmail.com>
2026-07-13 15:28:48 -0500

    add: `metric/LanguageCount` and `InvokeCli.Git.LsTree.Files`

---
+++ Commands/Private/cli/git/git-ls-tree.ps1
@@ -0,0 +1,57 @@
+﻿function InvokeCli.Git.LsTree.Files {
+    <#
+    .SYNOPSIS
+        (internal) Invoke native git ls-tree to list files
+    .EXAMPLE
+        InvokeCli.Git.LsTree.Files -Repo 'https://github.com/owner/repo.git'
+    #>
+    # [Alias('InvokeCli.Git.LsTree.Files')]
+    [CmdletBinding()]
+    param(
+        # root directory to clone under. '/cloned-repos' would clone to '/cloned-repos/owner/repository'
+        [Parameter(Mandatory)]
+        [Alias('Path', 'PSPath', 'GitRepo', 'RepoRoot')]
+        [string] $GitRepositoryPath,
+
+        # default uses 'ls-tree --full-tree'
+        [switch] $WithoutIncludeFullTree
+    )
+
+    # Run real git with args:
+    #region Invoke Real Git Args
+    $binGit = Get-Command -CommandType Application -Name 'git' -ea 'Stop' -TotalCount 1
+    [Collections.Generic.List[object]] $gitArgs = @(
+        '-C'
+        (Get-Item -ea 'stop' $GitRepositoryPath)
+        'ls-tree'
+        '-r'
+        'HEAD'
+        if( $WithoutIncludeFullTree ) { '--full-tree' }
+        '--name-only'
+    )
+
+    $gitArgs
+        | Join-String -sep ' ' -op 'invoke ''git'' => '
+        | Write-Verbose
+
+    $results = & $binGit @gitArgs
+
+    # note: slow because of the provider, but,
+    $instances = $results | gi | % Extension
+    $found = ($instances | Get-item | % Extension ) | Group-Object -NoElement | Sort count -Descending
+
+    $summary = $found.GetEnumerator() | %{
+        $extension = $_.Name -replace '^\.'
+        $count    = $_.Count
+        [pscustomobject][ordered]@{
+            PSTYpeName = 'GitServe.Metric.LanguageCount'
+            Extension  = $extension
+            Count      = $count
+            KeyId      = $extension
+        }
+    }
+
+
+    , @( $summary )
+    #endregion Invoke Real Git Args
+}
---
+++ Commands/Public/metric/LanguageCount.ps1
@@ -0,0 +1,52 @@
+﻿function Metric-GitServeLanguageCount {
+    <#
+    .SYNOPSIS
+        Which languages are used in a repo
+    .NOTES
+        Expects input type: 'git.log'
+    .EXAMPLE
+        git log | Metric-CommitCount
+        git log | Metric-GitServeCommitCount -Period month
+    .EXAMPLE
+        Use-Git -GitArg 'log', '-n', 4, '-C', $path | GitServe.Metric.CommitCount
+    #>
+    [Alias('GitServe.Metric.CommitCount')]
+    [OutputType(
+        '[System.Collections.Generic.SortedDictionary[string,object]]'
+    )]
+    [CmdletBinding()]
+    param(
+        # Path of git repo
+        [Parameter(Mandatory)]
+        [Alias('BaseDir', 'Repository', 'Path')]
+        [string] $GitRepositoryPath
+    )
+    begin {
+        # ( git.exe -C (gi .\Azure\) ls-tree -r HEAD --full-tree --name-only ).count
+        # todo: refactor generic
+    }
+    process {
+        $key = __toKeyId $InputObject
+        if( -not $metric.ContainsKey( $key ) ) {
+            $initialValue = [pscustomobject][ordered]@{
+                PSTYpeName  = 'GitServe.Metric.CommitCount'
+                DateDisplay  = $CommitDate.ToString( $dateDisplayFormat )
+                GitUserName = $GitUserName
+                CommitCount = 1
+                Year        = $CommitDate.Year
+                Month       = $CommitDate.Month
+                CommitDate  = $CommitDate
+                KeyId       = $key
+            }
+            $metric[ $key ] = $initialValue
+        } else {
+            $metric[ $key ].CommitCount += 1
+        }
+    }
+    end {
+        # if( $IncludeMissingDates ) {
+        #     # add any missing dates as explicit 0. Dates are based on the selected $period type. year/month/day/etc.
+        # }
+        ,@( $metric.Values )
+    }
+}
