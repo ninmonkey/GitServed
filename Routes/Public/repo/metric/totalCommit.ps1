fc7ec18846df2bf83ebc20e0968be3a683f047d4
Jake Bolton <ninmonkeys@gmail.com>
2026-07-27 13:48:59 -0500

    clean: document `/repo/metric/totalcommit` output datatypes

--- Routes/Public/repo/metric/totalCommit.ps1
+++ Routes/Public/repo/metric/totalCommit.ps1
@@ -10,23 +10,33 @@
         since  - "2.months"
         after  - '2024-01-01'
         before - '2024-01-01'
+
+    example record:
+        PSTypeName = 'GitServe.Route.Repo.Metric.TotalCommit'
+
+        [string] XAxisKey  : It is the same thing as the DateDisplayString
+        [int] TotalCommits : Aggregate in this period across all authors
+        [string] RepoName  : OwnerRepo pairs
+        [date] CommitDate  : [datetime] of the first CommitDate record in the group
+        [string[]] Authors : Aggregate list of all authors across the period
+
     .EXAMPLE
-        irm 'http://127.0.0.1:3001/repo/metric/commitcount?name=BurntSushi/ripgrep'
-        irm 'http://127.0.0.1:3001/repo/metric/commitcount?name=BurntSushi/ripgrep&period=month'
-        irm 'http://127.0.0.1:3001/repo/metric/commitcount?name=BurntSushi/ripgrep&period=day'
-        irm 'http://127.0.0.1:3001/repo/metric/commitcount?name=BurntSushi/ripgrep&period=year'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&period=month'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&period=day'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&period=year'
     .EXAMPLE
-        irm 'http://127.0.0.1:3001/repo/metric/commitcount?name=BurntSushi/ripgrep&since=2.months'
-        irm 'http://127.0.0.1:3001/repo/metric/commitcount?name=BurntSushi/ripgrep&after=2024-01-01'
-        irm 'http://127.0.0.1:3001/repo/metric/commitcount?name=BurntSushi/ripgrep&before=2026-01-01'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&since=2.months'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&after=2024-01-01'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&before=2026-01-01'
     .example
         # multiple filters
-        irm 'http://127.0.0.1:3001/repo/metric/commitcount?name=startautomating/ezout&after=2024-01-01&before=2024-09-04'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=startautomating/ezout&after=2024-01-01&before=2024-09-04'
     .EXAMPLE
     .LINK
         GitServe\Metric-GitServeCommitCount
     #>
-    [OutputType( 'GitServe.Route.Repo.Metric.Commit' )]
+    [OutputType( 'GitServe.Route.Repo.Metric.TotalCommit' )]
     [Alias('GitServe.Route.Metric.Commit')]
     [CmdletBinding()]
     param(
@@ -94,48 +104,34 @@
     $groupByPeriod =  $results | Group-Object -Prop { $_.CommitDate.ToString('yyyy-MM-dd') }
     [string[]] $datePeriodKeys = $groupByPeriod.Name
 
+    # insert numerically sorted list of strings in the order
     $dateAccum = [ordered]@{}
     foreach( $name in $datePeriodKeys ) {
-        @{
-            CommitDate = $Null
-            XAxisKey = $name
-            CommitCount = 0
-            RepoName = '<RepoName>'
-        }
+        $dateAccum[ $name ] = [ordered]@{}
     }
 
-
-    function _accumRecord {
-        # merge existing and new values
-        param(
-            [string] $KeyName,
-            [object] $Record
-        )
-    }
-
+    # aggregate group into a single record per this date period
     foreach( $groupRecord in $groupByPeriod ) {
         $curKey = $groupRecord.Name
-        $totalCommits = $groupRecord.Group | Measure-Object -Sum -Property 'CommitCount' | Select-Object -ExpandProperty Sum
+        $totalCommits = $groupRecord.Group
+            | Measure-Object -Sum -Property 'CommitCount'
+            | Select-Object -ExpandProperty Sum
+
         $firstDate = $groupRecord.Group
                 | Measure-object CommitDate -Minimum
                 | % Minimum
 
-
         $dateAccum[ $curKey ] = [pscustomobject]@{
-            CommitDate = $FirstDate
-            # XAxisKey = $curKey
-            TotalCommits = $totalCommits ?? 0
-            RepoName = '<RepoName>'
-            OwnerRepoName = $OwnerRepoPair
-            Authors = $groupRecord.Group.GitUserName | Sort-Object -Unique
+            PSTypeName   = 'GitServe.Route.Repo.Metric.TotalCommit'
+            XAxisKey     = $curKey                                               # typeof: [String] . It is the same thing as the DateDisplayString
+            TotalCommits = [int]( $totalCommits ?? 0)                            # typeof: [int]
+            RepoName     = $OwnerRepoPair
+            Authors      = $groupRecord.Group.GitUserName | Sort-Object -Unique
+            CommitDate   = $FirstDate                                            # typeof: [DateTime]
+            # OwnerRepoName = $OwnerRepoPair
         }
     }
-
-
-
     # $trash | Group -p { $_.CommitDate.Date } | sort  count
-
-
     # return ,$results
     return ,$dateAccum.Values
     #endregion Invoke Git Args
