854dd1e8bcb3e79ec8817b9e5bcd7802d3fa9735
Jake Bolton <ninmonkeys@gmail.com>
2026-07-31 18:16:36 -0500

    build: GitServe.psm1 v0.0.10
    ran: ./Build/Build.Module.ps1

--- GitServe.psm1
+++ GitServe.psm1
@@ -1,6 +1,6 @@
 <#
 .Description
-    Module built on: 2026-07-27 09:49:10Z
+    Module built on: 2026-07-31 18:16:23Z
 #>
 
 #region Module.Before.ps1
@@ -251,6 +251,56 @@ ${HtmlContent}
     $template -join [Environment]::NewLine
 }
 
+function Convert-GitServeQueryString {
+    <#
+    .SYNOPSIS
+        (internal) Parse request query strings. returns the named value collection
+    .NOTES
+        You can pass a raw Url or an HttpListenerRequest instance
+        This does not use [ValueFromPipelineByPropertyName]
+    #>
+    [Alias(
+        'ParseQueryString',
+        'GitServe.Convert.RequestQueryString'
+    )]
+    [OutputType(
+        [System.Collections.Specialized.NameValueCollection]
+    )]
+    [CmdletBinding()]
+    param(
+        # a raw Url or from an HttpListenerRequest instance
+        [Alias( 'HttpListenerRequest', 'Request', 'Listener', 'RawUrl', 'Url' )]
+        [Parameter(Mandatory, ValueFromPipeline )]
+        [object] $ListenerOrUrl
+    )
+
+    begin { }
+    process {
+        if ( $null -eq $ListenerOrUrl ) {
+            throw "ParseQueryString: Blank Url/ListenerRequest!"
+        }
+
+        if( $ListenerOrUrl -is [System.Net.HttpListenerRequest] ) {
+            [System.Net.HttpListenerRequest] $Listener = $ListenerOrUrl
+            [Collections.Specialized.NameValueCollection] $keyCollection =
+                [Web.HttpUtility]::ParseQueryString( $Listener.Url.Query.ToLower() )
+
+            return ,$keyCollection
+        }
+
+        if( $ListenerOrUrl -is [System.Uri]) {
+            [System.Uri] $Url = $ListenerOrUrl
+            [Collections.Specialized.NameValueCollection] $keyCollection =
+                [Web.HttpUtility]::ParseQueryString( $Url.Query.ToLower() )
+
+            return ,$keyCollection
+        }
+
+        throw "Unhandled Object type! $( ( $ListenerOrUrl )?.GetType() ) "    }
+    end {
+    }
+}
+
 function Set-CorsHeader {
     <#
     .SYNOPSIS
@@ -450,6 +500,7 @@ function Start-ListenLoop {
                     }
                     # Cache is stale, so invoke the Url Route
                     try {
+                        # future: 'ParseQueryString()'
                         $result = . $mappedCommand @cmdParams # *>&1
                     } catch {
                         $response.StatusCode = [System.Net.HttpStatusCode]::InternalServerError
@@ -1432,12 +1483,13 @@ function /repo/clone {
     #>
     [OutputType( 'GitServe.Route.Repo.Clone' )]
     param(
-        [Net.HttpListenerRequest] $Request
+        [object] $Request
     )
+
+    [Collections.Specialized.NameValueCollection] $parsedQuery = ParseQueryString $Request
 
     $Request | ConvertTo-Json -depth 1 -wa ignore | Write-Debug
 
-    $parsedQuery = [Web.HttpUtility]::ParseQueryString( $Request.Url.Query.ToLower() )
     [string] $gitUrl = @( $parsedQuery.GetValues('url') )[0]
 
     InvokeCli.Git.CloneRepo -Url $gitUrl -path (GetConfig.ClonedRepoRoot -First)
@@ -1486,11 +1538,11 @@ function /repo/metric/commit {
     param(
         # a request from the listen server
         [Parameter(Mandatory)]
-        [Net.HttpListenerRequest] $Request
+        [object] $Request
+
     )
     $endpointLabel = '/repo/metric/commit'
-    [Collections.Specialized.NameValueCollection] $parsedQuery =
-        [Web.HttpUtility]::ParseQueryString( $Request.Url.Query.ToLower() )
+    [Collections.Specialized.NameValueCollection] $parsedQuery = ParseQueryString $Request
 
     [string] $OwnerRepoPair = $parsedQuery.Get('name')
     [string] $Period = $parsedQuery.Get('period') ?? 'year'
@@ -1499,49 +1551,48 @@ function /repo/metric/commit {
         $ClonedRepoRoot = GetConfig.ClonedRepoRoot | Get-Item -ea 'stop'
         'RootPath: {0}' -f ( $ClonedRepoRoot ) | Write-Verbose
     }
+
+    #region Build Git Args
     $RepoPath = Join-Path $ClonedRepoRoot $OwnerRepoPair # todo(sanitization): use a better escape and match method
     if( ! ( Test-Path $RepoPath )) {
         "${endpointLabel} Error: Invalid OwnerRepoPair! '${OwnerRepoPair}'" | Write-Host -fore red
         throw "${endpointLabel} Error: Invalid OwnerRepoPair! '${OwnerRepoPair}'"
     }
-
-    #region Invoke Git Args
-    # $binGit = Get-Command -CommandType Application -Name 'git' -ea 'Stop' -TotalCount 1
     [Collections.Generic.List[object]] $gitArgs = @(
         'log'
-
-        if( $parsedQuery.Get('since') ) {
-            '--since={0}' -f $parsedQuery.Get('since')
-        }
-        if( $parsedQuery.Get('before') ) {
-            '--before={0}' -f $parsedQuery.Get('before')
-        }
-        if( $parsedQuery.Get('after') ) {
-            '--after={0}' -f $parsedQuery.Get('after')
-        }
-
-        # '-n'
-        # '100'
-        '-C'
-        $RepoPath # Note, ugit requires '-C' to be the final param, git native requires first
     )
 
-    $gitArgs
-        | Join-String -sep ' ' -op 'Clone: invoke ''git'' => '
-        | Write-Verbose
+    $SelectProperty = 'CommitDate', 'GitUserName', 'Date', 'Scope', 'CommitType', 'Merged', 'CommitHash', 'Trailer', 'Trailers'
+    $UGit_splat = @{
+        FromPath = $RepoPath
+        GitArgList = $gitArgs
+    }
+    if( $parsedQuery.Get('since') ) {
+        $UGit_splat['since'] = $parsedQuery.Get('since')
+    }
+    if( $parsedQuery.Get('before') ) {
+        $UGit_splat['before'] = $parsedQuery.Get('before')
+    }
+    if( $parsedQuery.Get('after') ) {
+        $UGit_splat['after'] = $parsedQuery.Get('after')
+    }
 
+    #endregion Build Git Args
+    #region Invoke Git Args
     try {
-        $SelectProperty = 'CommitDate', 'GitUserName', 'Date', 'Scope', 'CommitType', 'Merged', 'CommitHash', 'Trailer', 'Trailers'
-        [object[]] $results = Use-git -GitArg $gitArgs
+
+        [object[]] $results = Invoke-GitServeUGit @UGit_splat
+            | Select-Object -Property $SelectProperty
             | GitServe.Metric.CommitCount -Period $Period
-            # | Select-Object -Property $SelectProperty
-    } catch {
+    }
+    catch {
         "${endpointLabel} Error: Failed to get logs for '${OwnerRepoPair}' => $($_.Exception.Message)"
-            | Write-Host
+        | Write-Host
         "${endpointLabel} Error: Failed to get logs for '${OwnerRepoPair}' => $($_.Exception.Message)"
-            | Write-Error
+        | Write-Error
     }
     finally { }
+
 
     return ,$results
     #endregion Invoke Git Args
@@ -1577,12 +1628,11 @@ function /repo/metric/language {
     param(
         # a request from the listen server
         [Parameter(Mandatory)]
-        [Net.HttpListenerRequest] $Request
+        [object] $Request
     )
     $endpointLabel = '/repo/metric/language'
-    [Collections.Specialized.NameValueCollection] $parsedQuery =
-        [Web.HttpUtility]::ParseQueryString( $Request.Url.Query.ToLower() )
 
+    [Collections.Specialized.NameValueCollection] $parsedQuery = ParseQueryString $Request
     [string] $OwnerRepoPair = $parsedQuery.Get('name')
 
     if ( [String]::IsNullOrWhitespace( $ClonedRepoRoot ) ) {
@@ -1703,11 +1753,10 @@ function /repo/log {
     param(
         # a request from the listen server
         [Parameter(Mandatory)]
-        [Net.HttpListenerRequest] $Request
+        [object] $Request
     )
     $endpointLabel = '/repo/log'
-    [Collections.Specialized.NameValueCollection] $parsedQuery =
-        [Web.HttpUtility]::ParseQueryString( $Request.Url.Query.ToLower() )
+    [Collections.Specialized.NameValueCollection] $parsedQuery = ParseQueryString $Request
 
     #region Build Git Args
     [string] $OwnerRepoPair = $parsedQuery.Get('name')
@@ -1723,7 +1772,6 @@ function /repo/log {
         throw "${endpointLabel} Error: Invalid OwnerRepoPair! '${OwnerRepoPair}'"
     }
     # build git limiting args, which are common across ugit and git
-    #endregion Build Git Args
 
     [Collections.Generic.List[object]] $gitArgs = @(
         'log'
@@ -1747,6 +1795,7 @@ function /repo/log {
     if( $parsedQuery.Get('after') ) {
         $UGit_splat['after'] = $parsedQuery.Get('after')
     }
+    #endregion Build Git Args
 
     #region Invoke Git
     try {
@@ -1762,6 +1811,146 @@ function /repo/log {
     }
     return $results
     #endregion Invoke Git
+}
+
+function /repo/metric/totalcommit {
+    <#
+    .SYNOPSIS
+        Number of commits grouped and sorted by: "<CommitDate> Descending
+    .DESCRIPTION
+        You get one single aggregated record for each **date period**
+
+    Query Parameters:
+        name   - Short repo name like "BurntSushi/ripgrep"
+        since  - "2.months"
+        after  - '2024-01-01'
+        before - '2024-01-01'
+
+    example record:
+        PSTypeName = 'GitServe.Route.Repo.Metric.TotalCommit'
+
+        [string] XAxisKey  : It is the same thing as the DateDisplayString
+        [int] TotalCommits : Aggregate in this period across all authors
+        [string] RepoName  : OwnerRepo pairs
+        [date] CommitDate  : [datetime] of the first CommitDate record in the group
+        [string[]] Authors : Aggregate list of all authors across the period
+
+    .EXAMPLE
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&period=month'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&period=day'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&period=year'
+    .EXAMPLE
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&since=2.months'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&after=2024-01-01'
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=BurntSushi/ripgrep&before=2026-01-01'
+    .example
+        # multiple filters
+        irm 'http://127.0.0.1:3001/repo/metric/totalcommit?name=startautomating/ezout&after=2024-01-01&before=2024-09-04'
+    .EXAMPLE
+    .LINK
+        GitServe\Metric-GitServeCommitCount
+    #>
+    [OutputType( 'GitServe.Route.Repo.Metric.TotalCommit' )]
+    [Alias('GitServe.Route.Metric.Commit')]
+    [CmdletBinding()]
+    param(
+        # a request from the listen server
+        [Parameter(Mandatory)]
+        [object] $Request
+    )
+
+
+    $endpointLabel = '/repo/metric/commit'
+    [Collections.Specialized.NameValueCollection] $parsedQuery = ParseQueryString $Request
+
+    [string] $OwnerRepoPair = $parsedQuery.Get('name')
+    [string] $Period = $parsedQuery.Get('period') ?? 'year'
+
+    if ( [String]::IsNullOrWhitespace( $ClonedRepoRoot ) ) {
+        $ClonedRepoRoot = GetConfig.ClonedRepoRoot | Get-Item -ea 'stop'
+        'RootPath: {0}' -f ( $ClonedRepoRoot ) | Write-Verbose
+    }
+
+    #region Build Git Args
+    $RepoPath = Join-Path $ClonedRepoRoot $OwnerRepoPair # todo(sanitization): use a better escape and match method
+    if( ! ( Test-Path $RepoPath )) {
+        "${endpointLabel} Error: Invalid OwnerRepoPair! '${OwnerRepoPair}'" | Write-Host -fore red
+        throw "${endpointLabel} Error: Invalid OwnerRepoPair! '${OwnerRepoPair}'"
+    }
+    [Collections.Generic.List[object]] $gitArgs = @(
+        'log'
+    )
+
+    $SelectProperty = 'CommitDate', 'GitUserName', 'Date', 'Scope', 'CommitType', 'Merged', 'CommitHash', 'Trailer', 'Trailers'
+    $UGit_splat = @{
+        FromPath = $RepoPath
+        GitArgList = $gitArgs
+    }
+    if( $parsedQuery.Get('since') ) {
+        $UGit_splat['since'] = $parsedQuery.Get('since')
+    }
+    if( $parsedQuery.Get('before') ) {
+        $UGit_splat['before'] = $parsedQuery.Get('before')
+    }
+    if( $parsedQuery.Get('after') ) {
+        $UGit_splat['after'] = $parsedQuery.Get('after')
+    }
+
+    #endregion Build Git Args
+    #region Invoke Git Args
+    try {
+
+        [object[]] $results = Invoke-GitServeUGit @UGit_splat
+            | Select-Object -Property $SelectProperty
+            | GitServe.Metric.CommitCount -Period $Period
+    }
+    catch {
+        "${endpointLabel} Error: Failed to get logs for '${OwnerRepoPair}' => $($_.Exception.Message)"
+        | Write-Host
+        "${endpointLabel} Error: Failed to get logs for '${OwnerRepoPair}' => $($_.Exception.Message)"
+        | Write-Error
+    }
+    finally { }
+
+    # todo(performance): redundant operations here
+
+    # first determine date dimension keys. Insert into sorted hashtable in-order
+    $results = $results | Sort-Object CommitDate
+    $groupByPeriod =  $results | Group-Object -Prop { $_.CommitDate.ToString('yyyy-MM-dd') }
+    [string[]] $datePeriodKeys = $groupByPeriod.Name
+
+    # insert numerically sorted list of strings in the order
+    $dateAccum = [ordered]@{}
+    foreach( $name in $datePeriodKeys ) {
+        $dateAccum[ $name ] = [ordered]@{}
+    }
+
+    # aggregate group into a single record per this date period
+    foreach( $groupRecord in $groupByPeriod ) {
+        $curKey = $groupRecord.Name
+        $totalCommits = $groupRecord.Group
+            | Measure-Object -Sum -Property 'CommitCount'
+            | Select-Object -ExpandProperty Sum
+
+        $firstDate = $groupRecord.Group
+                | Measure-object CommitDate -Minimum
+                | % Minimum
+
+        $dateAccum[ $curKey ] = [pscustomobject]@{
+            PSTypeName   = 'GitServe.Route.Repo.Metric.TotalCommit'
+            XAxisKey     = $curKey                                               # typeof: [String] . It is the same thing as the DateDisplayString
+            TotalCommits = [int]( $totalCommits ?? 0)                            # typeof: [int]
+            RepoName     = $OwnerRepoPair
+            Authors      = $groupRecord.Group.GitUserName | Sort-Object -Unique
+            CommitDate   = $FirstDate                                            # typeof: [DateTime]
+            # OwnerRepoName = $OwnerRepoPair
+        }
+    }
+    # $trash | Group -p { $_.CommitDate.Date } | sort  count
+    # return ,$results
+    return ,$dateAccum.Values
+    #endregion Invoke Git Args
 }
 
 
