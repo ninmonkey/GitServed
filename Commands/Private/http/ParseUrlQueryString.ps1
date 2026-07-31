7f1115469e2a04cf2c34bcf60f9a52e7b7c1d0bd
Jake Bolton <ninmonkeys@gmail.com>
2026-07-31 17:48:42 -0500

    clean: simplfiy `ParseQueryString` parameter binding

--- Commands/Private/http/ParseUrlQueryString.ps1
+++ Commands/Private/http/ParseUrlQueryString.ps1
@@ -3,50 +3,43 @@
     .SYNOPSIS
         (internal) Parse request query strings. returns the named value collection
     .NOTES
-        Currently not throwing via parameterbinding
+        You can pass a raw Url or an HttpListenerRequest instance
+        This does not use [ValueFromPipelineByPropertyName]
     #>
     [OutputType(
         [System.Collections.Specialized.NameValueCollection]
     )]
     [CmdletBinding()]
     param(
-        # From an HttpListener
-        [Alias('HttpListener' )]
-        [Parameter(
-            ParameterSetName = 'FromListener',
-            ValueFromPipeline
-        )]
-        [Net.HttpListenerRequest] $Listener,
-
-        # from a raw Url. No server.
-        [Alias('RequestUrl')]
-        [Parameter(
-            ParameterSetName = 'FromUrl',
-            ValueFromPipeline
-        )]
-        [Uri] $Url
+        # a raw Url or from an HttpListenerRequest instance
+        [Alias( 'HttpListenerRequest', 'Request', 'Listener', 'RawUrl', 'Url' )]
+        [Parameter(Mandatory, ValueFromPipeline )]
+        [object] $ListenerOrUrl
     )
 
     begin { }
     process {
-        if ( -not $Listener -and -not $Url ) {
+        if ( $null -eq $ListenerOrUrl ) {
             throw "ParseQueryString: Blank Url/ListenerRequest!"
         }
 
-        if( $null -ne $Listener ) {
+        if( $ListenerOrUrl -is [System.Net.HttpListenerRequest] ) {
+            [System.Net.HttpListenerRequest] $Listener = $ListenerOrUrl
             [Collections.Specialized.NameValueCollection] $keyCollection =
                 [Web.HttpUtility]::ParseQueryString( $Listener.Url.Query.ToLower() )
 
             return $keyCollection
         }
 
+        if( $ListenerOrUrl -is [System.Uri]) {
+            [System.Uri] $Url = $ListenerOrUrl
+            [Collections.Specialized.NameValueCollection] $keyCollection =
+                [Web.HttpUtility]::ParseQueryString( $Url.Query.ToLower() )
 
-        # otherwise it's a non-blank url
-        [Collections.Specialized.NameValueCollection] $keyCollection =
-                [Web.HttpUtility]::ParseQueryString( $Url.Query.toLower() )
-        return $keyCollection
+            return $keyCollection
+        }
 
-    }
+        throw "Unhandled Object type! $( ( $ListenerOrUrl )?.GetType() ) "    }
     end {
     }
 }
