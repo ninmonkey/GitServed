c37ea3bc850a4e52abd72bd7a1e38e2a3bf2367b
Jake Bolton <ninmonkeys@gmail.com>
2026-08-03 16:08:33 -0500

    clean: strip old code and cleanup naming
    fix test formatting

--- Tests/Private/DateDimension.tests.ps1
+++ Tests/Private/DateDimension.tests.ps1
@@ -5,17 +5,19 @@
     # rebuild, and import latest
     $ModuleName= 'GitServe'
     $ModuleRoot     = Join-path $PSScriptRoot '../..'
-    $ModuleManifest = Join-Path $ModuleRoot "${ModuleName}.psd1" # ie: '/GitServe.psd1'
+    $ModuleManifest = gi -ea 'stop' ( Join-Path $ModuleRoot "${ModuleName}.psd1" ) # ie: '/GitServe.psd1'
     $ToBuild        = Join-Path $ModuleRoot '/Build/Build.Module.ps1' | Get-Item -ea 'stop'
-    $TestUtilsPs1  = Join-Path $ModuleRoot '/Tests/test_utils.psm1'
+    $TestUtilsPs1  = Join-Path $ModuleRoot '/Tests/test_utils.ps1'
 
     & $ToBuild
-    'Done' | Write-Host -bg 'green'
+    'Build Done' | Write-Host -bg 'green'
+    # . $TestUtilsPs1
+    'Pester imports: Done' | Write-Host -bg 'green'
 
-    Remove-module 'GitServe' -ea 'ignore'
+    # Remove-module 'GitServe' -ea 'ignore'
     @(
         Import-Module  -PassThru -Force $ModuleManifest
-        Import-Module  -PassThru -Force $TestUtilsPs1
+        # Import-Module  -PassThru -Force $TestUtilsPs1
     )
         | Join-String -p { $_.Name, $_.Version -join ': ' } -op "importing:...`n" -f "`n - {0}"
         | Pansies\Write-Host -fore 'goldenrod'
@@ -25,16 +27,15 @@ Describe 'Get-NextDatePeriod' {
     Context 'Get Periods' {
         It 'Current: <CurrentDate>, Period: <Period> is <ExpectedStr>' -ForEach @(
             @{
-                CurrentDate = [datetime]::ParseExact('2026-03-04', 'yyyy-MM-dd', [cultureinfo]::GetCultureInfo('en-us'))
+                CurrentDate =  Date.FromStr '2026-03-04'
                 Period      = 'month'
-                ExpectedStr = '2026-03-04'
+                ExpectedStr = '2026-04-04'
             }
-            # @{
-            #     CurrentDate = [datetime]::ParseExact('2026-03-07', 'yyyy-MM-dd', [cultureinfo]::GetCultureInfo('en-us'))
-            #     Period      = 'month'
-            #     ExpectedStr = '2026-03-04'
-            # }
-
+            @{
+                CurrentDate =  Date.FromStr '2026-07-04'
+                Period      = 'week'
+                ExpectedStr = '2026-07-11'
+            }
         ) {
             $datePeriod_splat = @{
                 CurrentDate = $CurrentDate
@@ -44,34 +45,10 @@ Describe 'Get-NextDatePeriod' {
                 $datePeriod_splat['MaxDate'] = $MaxDate
 
             }
-            GitServe.Get-NextDatePeriod @datePeriod_splat
-                | % {  $_.ToString('yyyy-MM-dd', ([cultureinfo]::GetCultureInfo('en-us') ) ) }
-                | Should -Be $ExpectedStr
+            # clean(future): use the newer should verb syntax
+            $newDate = GitServe.Get-NextDatePeriod @datePeriod_splat
+            Date.Str $newDate | Should -Be $ExpectedStr
 
         }
     }
 }
-# This is verbose, but the edge cases should be defined
-# Describe "Test: IsBlank" -tag 'internal' {
-#     Context 'Is.Null' {
-#         It 'from Param: <value> is <expected>' -ForEach @(
-#             @{ Value = $null ; Expected = $true }
-#             @{ Value = ''    ; Expected = $false }
-#             @{ Value = ' '   ; Expected = $false }
-#         ) {
-#             InModuleScope 'Mintils' -Parameters $_ {
-#                 _Is.Null $Value   | Should -BeExactly $Expected
-#             }
-#         }
-#         It 'from Pipeline: <value> is <expected>' -ForEach @(
-#             @{ Value = $null ; Expected = $true }
-#             @{ Value = ''    ; Expected = $false }
-#             @{ Value = ' '   ; Expected = $false }
-#         ) {
-#             InModuleScope 'Mintils' -Parameters $_ {
-#                 $value | _Is.Null | Should -BeExactly $Expected
-#             }
-#         }
-#     }
-# }
-#
