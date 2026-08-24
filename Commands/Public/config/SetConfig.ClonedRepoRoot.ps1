018a872884d386329987542994e4a96ac778da00
Jake Bolton <ninmonkeys@gmail.com>
2026-08-24 16:11:26 -0500

    clean: Changing ClonedRepoRoot invalidates the `JsonCacheRepoList` cache

--- Commands/Public/config/SetConfig.ClonedRepoRoot.ps1
+++ Commands/Public/config/SetConfig.ClonedRepoRoot.ps1
@@ -2,6 +2,10 @@
     <#
     .synopsis
         Set app configuration for root directories to search ( ie: local, vs docker, etc )
+    .EXAMPLE
+        # using one or more custom paths
+        GitServe.Set-ConfigRepoRoot -Path 'C:\MyRepos', 'C:\MoreRepos'
+        GitServe.Repo.List | Ft # shows updated repos
     .DESCRIPTION
         Set root directories for cloned repos.
     #>
@@ -17,4 +21,7 @@
 
     # Clear cached repos since the path[s] have changed
     Clear-ResponseCacheKey -Key '/repo/list' -Verbose:$false
+
+    # clear JsonCache for for GitServe.Repo.List
+    Remove-Item -ea ignore -LiteralPath $script:ModuleState.JsonCacheRepoList
 }
