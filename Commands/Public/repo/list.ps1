e7c82f7658165afcd62ed51d6d89da1aa5d835e1
Jake Bolton <ninmonkeys@gmail.com>
2026-08-24 13:18:46 -0500

    add: cache `GitServe.Repo.List`
    additional debug logging

--- Commands/Public/repo/list.ps1
+++ Commands/Public/repo/list.ps1
@@ -5,13 +5,16 @@
     .DESCRIPTION
     .notes
     .example
-        >
+        GitServe.Repo.List
+    .example
+        # force updating the repo listing
+        GitServe.Repo.List -WithoutCache
     #>
     [Alias('GitServe.Repo.List')]
     [OutputType( 'GitServe.Route.Repo.List' )]
     [CmdletBinding()]
     param(
-        # Force refresh, clear repo listing cache
+        # Force refresh, clear repo listing cache. Saves result to cache file.
         [Alias('Force')]
         [switch] $WithoutCache
     )
@@ -20,28 +23,35 @@
     $findGitRepos = Get-ChildItem $searchRoot -Filter '.git' -Directory -Force -Recurse | ForEach-Object Parent
     $delim = "`u{2400}" # unique, but safe to print delimiter
 
+    $outputTypeName = 'GitServe.Route.Repo.List'
+
     #region load cache
-    $JsonCache = $script:ModuleState.JsonCacheRepoList
+    $JsonCachePath = $script:ModuleState.JsonCacheRepoList
     $cache = $null
     [Collections.Generic.List[object]] $records = @()
 
     if( $WithoutCache ) {
-        Remove-Item -LiteralPath $JsonCache
+        '$WithoutCache, deleting: ' | Write-Verbose
+        Remove-Item -LiteralPath $JsonCachePath
     }
 
     if( -not $WithoutCache ) {
-        $JsonCache | Join-String -op 'Using Cache: ' | Write-Verbose
-        $cache = Get-Content $JsonCache | ConvertFrom-Json
-        # valid results?
+        $JsonCachePath | Join-String -op 'Using Cache: ' | Write-Verbose
+        $cache = Get-Content $JsonCachePath -ea ignore | ConvertFrom-Json
+        $cache.RepoList.Count | Join-String -op '$cache.RepoList.Count: ' | Write-Verbose
+        # if valid results, emit correct type names
         if( $cache.RepoList ) {
-            write-warning 'ensure PSTYpeName'
-            $records = $cache.RepoList
+            $records = $cache.RepoList | %{
+                $_.PSObject.TypeNames.Insert(0, $outputTypeName )
+                $_
+            }
         }
     }
     #endregion load cache
 
+    $records.count | Join-String -op 'records loaded records from cache? ' | Write-Verbose
 
-    if( $WithoutCache -or $records.count -eq 0 ) {
+    if( $WithoutCache -or ( $records.count -eq 0 ) ) {
         #region calculate new return value
         # cache was either invalid or was disabled
         'Calculating fresh repo list' | Write-Verbose
@@ -79,12 +89,23 @@
         #endregion calculate new return value
     }
 
-    # save cache
-    @{
-        LastUpdate = [datetime]::Now
-        RepoList = @( $records )
-    }   | ConvertTo-Json
-        | Set-Content -Encoding utf8 -Path $JsonCache
+    $records.count | Join-String -op 'final $records.count: ' | Write-Verbose
+
+    # save cache if any records are found
+    if( $records.count -gt 0  ) {
+        $JsonCachePath | Join-String -op 'Writing: ' | Write-Verbose
+        @{
+            LastUpdate = [datetime]::Now
+            RepoList = @( $records )
+        }   | ConvertTo-Json
+            | Set-Content -Encoding utf8 -Path $JsonCachePath
+    }
+
+    # Always remove file if records are empty
+    if( $records.count -eq 0 ) {
+        $JsonCachePath | Join-String -op 'Records count == 0, deleting: ' | Write-Verbose
+        Remove-Item -LiteralPath $JsonCachePath
+    }
 
 
     return $records
