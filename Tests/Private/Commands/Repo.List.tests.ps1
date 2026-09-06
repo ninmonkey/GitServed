51c8f7cf4a624994bde6b11c8662b3dfbc47aff1
Jake Bolton <ninmonkeys@gmail.com>
2026-09-06 10:12:46 -0500

    create first pester6 test for Repo.List ( ref: #42 )
    ensures clone repo path exists, and has the right repo cloned

---
+++ Tests/Private/Commands/Repo.List.tests.ps1
@@ -0,0 +1,42 @@
+﻿#requires -Modules Pansies, Pester
+#requires -PSEdition Core
+
+$PSStyle.OutputRendering = 'ansi'
+
+BeforeAll {
+    $WorkspaceRoot = Get-Item -ea 'stop' ( Join-Path $PSScriptRoot '../../..' )
+    # load newest build
+    Import-Module -Force -PassThru ( Join-Path $WorkspaceRoot 'GitServe.psd1' )
+    | Join-String -op 'Import: ' -p { $_.Name, $_.Version } | Write-Host -bg 'blue'
+
+    # Load known repos for testing
+    $SourceRepos = Get-Item -ea 'stop' 'C:\GitLoggerApp\Testcase-ClonedRepos'
+    GitServe.Set-ConfigRepoRoot -Path $SourceRepos
+    # GitServe.Repo.List -WithoutCache # force cache is right for this instance
+}
+
+Describe 'GitServe.Repo.List' {
+    Context 'Required Testing Data' {
+        # todo(pester): move to top level file that all subdirectories require this test before runnign
+        It 'has repo: <OwnerRepoPair>' -ForEach @(
+            @{ OwnerRepoPair = 'burntsushi/ripgrep' }
+        ) {
+            GitServe.Repo.List
+            | Should-Any { $_.OwnerRepoPair -eq $OwnerRepoPair } -Because 'The required testing repo should exist'
+        }
+    }
+    It 'Correct Types' {
+        # It 'typeof' {
+        #     $repos | GitServe.Repo.List
+        #     $resp[0] | Should -BeOfType 'GitServe.Route.Repo.List'
+        #     $resp[0] | Should -BeOfType 'GitServe.wrong.List'
+        # }
+        # It 'A' {
+            $repos = GitServe.Repo.List
+            $repos.Count | Should -Not -Be 0
+            # $repos.Count | Should -Be 99
+            # $repos.Count | Should -Be 99
+            # $repos.Count | Should -Be 4
+        # }
+    }
+}
