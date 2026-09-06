9034575254bddf6e17cfc09ad659356d767bdc57
Jake Bolton <ninmonkeys@gmail.com>
2026-09-06 11:13:50 -0500

    add CloneRepoIfMissing for #42

--- Tests/Public/Repo/Repo.List.Tests.ps1
+++ Tests/Public/Repo/Repo.List.Tests.ps1
@@ -15,23 +15,68 @@ BeforeAll {
 
 
     # Load known repos for testing
-    $SourceRepos = Get-Item -ea 'stop' 'C:\GitLoggerApp\Testcase-ClonedRepos'
-    GitServe.Set-ConfigRepoRoot -Path $SourceRepos
+    $SourceReposRoot = Get-Item -ea 'stop' 'C:\GitLoggerApp\Testcase-ClonedRepos'
+    GitServe.Set-ConfigRepoRoot -Path $SourceReposRoot
     # GitServe.Repo.List -WithoutCache # force cache is right for this instance
 
     function CloneRepoIfMissing {
+        <#
+        .SYNOPSIS
+            ensure repo exists at "<SourceReposRoot/Owner/Repo>" otherwise clone it
+        #>
         param(
-            [string] $OwnerRepoPair
-        )
-        $found = GitServe.Repo.List | ? OwnerRepoPair -eq $OwnerRepoPair
-        if( $Found ) { return }
-        "Missing ${ownerRepPair}... Cloning..." | Write-Host -fg 'salmon'
+            [Parameter(Mandatory)]
+            [string] $OwnerRepoPair,
 
-        'burntsushi/ripgrep'
+            # which exact hash to checkout on
+            [Parameter(Mandatory)]
+            [string] $CheckoutHash,
+
+            # what to clone
+            [Parameter(Mandatory)]
+            [uri]$CloneUrl,
+
+            # folder to clone under
+            [Parameter(Mandatory)]
+            [string] $RepoRoot
+
+        )
+        $found = GitServe.Repo.List -WithoutCache | ? OwnerRepoPair -eq $OwnerRepoPair
+        if( $Found ) { return }
+
+        "Missing ${ownerRepPair}... Cloning..." | Log.Warn
+        $ownerName, $repoName = $OwnerRepoPair -split '/', 2
+
+        $cloneParentDir = Join-Path $RepoRoot $ownerName
+        mkdir $cloneParentDir -ea Ignore -Confirm:$False
+
+        GitServe.Invoke-RealGit -Frompath $cloneParentDir -GitArgList @(
+            'clone'
+            $CloneUrl
+        )
+
+        $repoFullPath = Join-path $cloneParentDir $repoName
+        "Cloned to: ${repoFullPath}" | Log.Info
+
+        GitServe.Invoke-RealGit -FromPath $repoFullPath -GitArgList @(
+            'checkout'
+            $CheckoutHash
+        ) | Write-Verbose
+
+        "Checked hash: ${CheckoutHash}" | Log.Info
+
+        # force clear cache because the clone uses raw git commands to clone, and because of commit hash change
+        GitServe.Repo.List -WithoutCache
     }
 
-    CloneRepoIfMissing
-    # 3fce3b5bb0236da2df6d99672afb8a719642eca7
+    $cloneRepoIfMissingSplat = @{
+        CloneUrl      = 'https://github.com/burntsushi/ripgrep'
+        OwnerRepoPair = 'burntsushi/ripgrep'
+        RepoRoot      = $SourceReposRoot
+        CheckoutHash  = '3fce3b5bb0236da2df6d99672afb8a719642eca7'
+    }
+
+    CloneRepoIfMissing @cloneRepoIfMissingSplat
 }
 
 Describe 'GitServe.Repo.List' {
@@ -46,7 +91,8 @@ Describe 'GitServe.Repo.List' {
         It 'cloned right date range' {
             $one = GitServe.Repo.List | ? OwnerRepoPair -eq 'burntsushi/ripgrep'
             $one.NewestCommitDate | Should-Be -Expected '2026-08-04' -because 'Tests require this date range'
-            $one.Remote | Should-Be -Expected 'https://github.com/BurntSushi/ripgrep.git'
+            $one.Remote | Should-MatchString -Expected 'https://github.com/BurntSushi/ripgrep' # .git suffix depends on clone command
+
         }
     }
     It 'Correct Types' {
