434e76c3ef7c30c7ef1d09ea92e61de89a6692ef
Jake Bolton <ninmonkeys@gmail.com>
2026-09-14 11:44:02 -0500

    add: Script runs GitServe with shared pester config

---
+++ Tests/scripts/Start-TestCaseGitServe.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.SYNOPSIS
+    Re-Import and host GitServe using TestCase Pester config
+#>
+param(
+    [string] $CloneRepoRoot = 'C:\GitLoggerApp\Testcase-ClonedRepos'
+)
+
+try { GitServe.Stop } catch { }
+$error.clear()
+Import-Module -ea 'stop' "${PSScriptRoot}/../../Gitserve.psd1" -Force -PassThru
+$curHost = GitServe.Get-ConfigHost
+GitServe.Set-ConfigRepoRoot -Path $CloneRepoRoot
+GitServe.Start -HostName $curHost.Host -Port $curHost.Port -PSHost
